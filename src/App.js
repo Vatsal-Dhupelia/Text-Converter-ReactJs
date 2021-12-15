d54714611b12234f7b0ed3a288967649ca1a7d08
Vatsal-Dhupelia <[vatsaldhupelia@gmail.com]>
2021-12-15 20:28:47 +0530

    Included Dark Mode Functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react'
+import Alert from './components/Alert';
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 3000);
+  }
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#212529';
+      showAlert("Dark Mode has been enabled", "success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white'; 
+      showAlert("Light Mode has been enabled", "success");
+    }
+  }
   return (
     <>
-    <Navbar title="TextUtils" aboutText="About TextUtils"/>
+    <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
     <div className="container my-3">
-      <TextForm heading="Enter your Text"/>
+      <TextForm showAlert={showAlert} heading="Enter your Text" mode={mode}/>
     </div>
   </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,10 +4,23 @@ export default function TextForm(props) {
     const handleUpClick = ()=>{
         let newText = text.toUpperCase();
         setText(newText)
+        props.showAlert("Converted to Uppercase!", "success")
     }
     const handleLoClick = ()=>{
         let newText = text.toLowerCase();
         setText(newText)
+        props.showAlert("Converted to Lowercase!", "success")
+    }
+    const handleClearClick = ()=>{
+        let newText = '';
+        setText(newText)
+        props.showAlert("Text Cleared!", "success")
+    }
+    const handleCopy = ()=>{
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        props.showAlert("Text copied to clipboard!", "success")
     }
     const handleOnChange = (event)=>{
         setText(event.target.value)
@@ -16,10 +29,10 @@ export default function TextForm(props) {
     const [text, setText] = useState('');
     return (
         <>
-            <div className="container">
+            <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
                 <h2>{props.heading}</h2>
                 <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                    <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#212529':'white', color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
                 </div>
                 <button className="btn btn-primary mx-2" onClick={handleUpClick}>
                     Convert to Uppercase
@@ -27,13 +40,19 @@ export default function TextForm(props) {
                 <button className="btn btn-primary mx-2" onClick={handleLoClick}>
                     Convert to Lowercase
                 </button>
+                <button className="btn btn-primary mx-2" onClick={handleClearClick}>
+                    Clear Text
+                </button>
+                <button className="btn btn-primary mx-2" onClick={handleCopy}>
+                    Copy Text
+                </button>
             </div>
-            <div className="container my-3">
+            <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
                 <h2>Your Text Summary</h2>
                 <p>{text.split(" ").length} words {text.length} characters</p>
                 <p>{0.008 * text.split(" ").length} Minutes Read</p>
                 <h3>Preview</h3>
-                <p>{text}</p>
+                <p>{text.length>0?text:"Enter text in the textarea to see its preview."}</p>
             </div>
         </>
     )
